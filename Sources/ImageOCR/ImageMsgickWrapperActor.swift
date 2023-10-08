e1e17927cf75011ce1f8f9b2c6da7b2ebc0093f3
John Corner <virus1993@icloud.com>
2023-10-08 21:31:19 +0800

    more flexble for ImageMsgickWrapperActor, it can change source now

--- Sources/ImageOCR/ImageMsgickWrapperActor.swift
+++ Sources/ImageOCR/ImageMsgickWrapperActor.swift
@@ -9,24 +9,31 @@ import Foundation
 
 @available(macOS 10.15.0, iOS 13, *)
 public actor ImageMsgickWrapperActor {
-    private let wrapper: ImageMsgickWrapper
+    private var wrapper: ImageMsgickWrapper?
+
+    public init() { }
     
     public init(_ source: ImageSourceProvider) {
         self.wrapper = ImageMsgickWrapper(source)
+    }
+
+    public func source(_ value: ImageSourceProvider) -> Self {
+        wrapper = ImageMsgickWrapper(value)
+        return self
     }
     
     /// 放大缩小图片
     /// - Parameter times: 放大倍数， 0-1.0
     @discardableResult
     public func scale(_ times: Double) throws -> Self {
-        try wrapper.scale(times)
+        try wrapper?.scale(times)
         return self
     }
     
     /// 二值化图片
     @discardableResult
     public func threshold() throws -> Self {
-        try wrapper.threshold()
+        try wrapper?.threshold()
         return self
     }
     
@@ -34,7 +41,7 @@ public actor ImageMsgickWrapperActor {
     /// - Parameter colorSpace: 需要转换到的色彩空间
     @discardableResult
     public func transformColorspace(_ colorSpace: ImageMagickColorSpace) throws -> Self {
-        try wrapper.transformColorspace(colorSpace)
+        try wrapper?.transformColorspace(colorSpace)
         return self
     }
     
@@ -43,11 +50,19 @@ public actor ImageMsgickWrapperActor {
     /// - Returns: 新文件路径
     @discardableResult
     public func export(to location: URL) throws -> URL {
-        try wrapper.export(to: location)
+        guard let wrapper = wrapper else {
+            logger.error("ImageMsgickWrapperActor export to \(location) failed, wrapper is nil")
+            throw InnerError.wandInvalid
+        }
+        return try wrapper.export(to: location)
     }
     
     /// 当前图片数据
     public func data() throws -> Data {
-        try wrapper.data()
+        guard let wrapper = wrapper else {
+            logger.error("ImageMsgickWrapperActor data failed, wrapper is nil")
+            throw InnerError.wandInvalid
+        }
+        return try wrapper.data()
     }
 }
